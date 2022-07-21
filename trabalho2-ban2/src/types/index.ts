7c217588bd713e1f45637c05ba755b52f0597e1b
gusscamargo <guga.power@hotmail.com>
2022-07-21 19:12:09 -0300

    add types

---
+++ trabalho2-ban2/src/types/index.ts
@@ -0,0 +1,101 @@
+import { Document } from "mongoose"
+
+export interface ResponseFuncs{
+    GET?: Function
+    POST?: Function
+    PUT?: Function
+    DELETE?: Function
+}
+
+// Feito
+export interface ICidade extends Document {
+    nome: string
+    uf: string
+}
+
+// Feito
+export interface IHotel extends Document { 
+    _id: string
+    nome: string
+    endereco: string
+    telefone: string
+    cidade: ICidade
+    empregados: [IEmpregado]
+    clientes: [ICliente]
+    quartos: [IQuarto]
+} 
+
+// Feito
+export interface IUsuario extends Document{
+    _id: string
+    email: string
+    senha: string
+    compararSenhas(senhaCandidata: string, next: (err: Error | null, same: boolean | null) => void): void
+}
+
+// Feito
+export interface IEmpregado extends Document {
+    _id: string
+    credencial: IUsuario
+    nome: string
+    endereco: string
+    telefone: string
+    funcao: [string]
+}
+
+// Feito
+export interface ICliente extends Document {
+    _id: string
+    credencial: IUsuario
+    nome: string
+    cpf: string
+    endereco: string
+    telefone: string
+    reservas: [IReserva]
+    estadias: [IEstadia]
+}
+
+// Feito
+export interface IQuarto extends Document{
+    _id: string
+    tipo: string
+    numero: number
+    andar: number
+    preco_pernoite: number
+}
+
+// Feito
+export interface IReserva extends Document{
+    _id: string
+    quarto: IQuarto
+    check_in: Date
+    check_out: Date
+    dia_reserva: Date
+    cama_extra: boolean
+    valor_total: number
+    valor_entrada: number
+    status: string
+}
+
+export interface IEstadia extends Document{
+    _id: string
+    quarto: IQuarto
+    check_in: Date
+    check_out: Date
+    valor: number
+    servicos: [IServico]
+}
+
+// Feito
+export interface IServico extends Document{
+    _id: string
+    empregado: {
+        _id: string
+        nome: string
+    }
+    valor: number
+    dia: Date
+    descricao: string
+}
+
+export type Catcher = (err: Error) => void
