876678d2aecdf5d777725277cfda8db5ef9f4677
gusscamargo <guga.power@hotmail.com>
2022-07-22 18:09:01 -0300

    Reorganização de configurações

--- trabalho2-ban2/app.ts
+++ trabalho2-ban2/app.ts
@@ -1,38 +1,25 @@
+import "dotenv/config"
 import express from "express"
-import session from "express-session"
 import { engine } from "express-handlebars"
-import {config as envConfig} from "dotenv"
 import cookieParser from "cookie-parser"
-import mongoose from "mongoose"
-import MongoStore from "connect-mongo"
 import Routes from "./src/routes"
 import path from "path"
-import { v4 as uuid } from "uuid"
-envConfig()
+import { mongoose, session, passport} from "./src/configs"
 
 
 const app = express()
 const port = process.env.PORT_SERVER || 3000
 
-const {
-    MONGODB_USER,
-    MONGODB_PASSWORD,
-    MONGODB_ANDRESS,
-    MONGODB_PORT
-} = process.env
-
-// Conect database
-const mongo_url: string = `mongodb://${MONGODB_USER}:${MONGODB_PASSWORD}@${MONGODB_ANDRESS}:${MONGODB_PORT}`
+// Mongo db
 mongoose
-    .connect(mongo_url)
-    .then(() => console.log("Conectado ao mongo"))
-    .catch(err => console.error(`Deu problema:\n${err}`))
 
 // Configs
 app.use(express.json())
 app.use(express.urlencoded({ extended: false}))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, '/public')))
+
+// Handlebars
 app.engine(".hbs", engine({
     extname: ".hbs",
     defaultLayout: "main",
@@ -42,18 +29,12 @@ app.engine(".hbs", engine({
 app.set("view engine", ".hbs")
 app.set("views", "./src/views/pages")
 
+
+
 // Session
-app.use(session({
-    store: MongoStore.create({ mongoUrl: mongo_url, dbName: "session" }),
-    secret: "Bando de dados 2",
-    genid: () => uuid(),
-    resave: false,
-    saveUninitialized: true,
-    cookie: {
-        secure: false,
-        maxAge: (3600000 * 24) * 7
-    },
-}))
+app.use(session)
+app.use(passport.initialize())
+app.use(passport.session())
 
 // Routes
 app.use("/hotel", Routes.Hotel)
---
+++ trabalho2-ban2/src/configs/index.ts
@@ -0,0 +1,11 @@
+import mongoose, {mongo_url} from "./mongoose";
+import session from "./session";
+import passport from "./passport";
+
+
+export {
+    mongoose,
+    mongo_url,
+    session,
+    passport
+}
